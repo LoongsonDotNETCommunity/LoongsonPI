d6fdb21427038d19f5152acb81b2a14d6df7c125
Juster Zhu <zhuzhen723723@outlook.com>
2024-06-14 22:58:46 +0800

    feature: add linux core

---
+++ linux/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 21 */
+/* SMP */
+#define UTS_MACHINE "loongarch64"
+#define UTS_VERSION "#21 SMP Fri Jun 14 11:36:36 CST 2024"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "dell"
+#define LINUX_COMPILER "gcc version 8.3.0 (LoongArch GNU toolchain rc1.2 (20230615))"
